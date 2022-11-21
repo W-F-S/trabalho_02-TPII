466f2392bc22b8f122318876de9feb9d6444f165
W-F-S <walkerf082@gmail.com>
2022-11-21 12:10:37 -0300

    modificando classe atendente

--- sistema/atendente.cpp
+++ sistema/atendente.cpp
@@ -13,20 +13,35 @@ Atendente::Atendente(QString nome, int cpf, QString email, int telefone, bool se
 }
 
 
-void Atendente::cadastrar_usuario(string nome, Sistema usuario)
+void Atendente::cadastrar_usuario(int tipo, Sistema usuario)
 {
+    string pasta;
+    //verificando o tipo de usuario a ser criado
+    switch
+    {
+        case 0:
+            pasta = "atendente";
+        break;
+        case 1:
+            pasta = "medico";
+        break;
+        case 2:
+            pasta = "paciente";
+        break;
+        default:
+            cout << "erro";
+            exit(1);
+        break;
+    }
 
+    //apenas atendentes gerais podem criar novas atendentes
     string localpath = "";
-    if(nome == "Secretaria" && !Atendente::secretaria_geral)
+    if(tipo == 0 && !Atendente::secretaria_geral)
     {
         cout<< "Erro, este usuário não tem permissão para criar um usuário" << endl;
     }
 
-
-    localpath = filesystem::current_path();
-
-
-
-
+    localpath = filesystem::current_path(); //coletando to caminho do arquivo atual
+    filesystem::create_directory(loacalpath+"/"+pasta);
 
 }
